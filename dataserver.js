64902ad2d5db16b3431ebc7d3b34f6f912f30cc9
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-11-03 11:42:27 -0700

    got basic express static server running

--- dataserver.js
+++ dataserver.js
@@ -1,1 +1,15 @@
-dataserver.js
+
+const express = require('express');
+
+const server = express();
+
+server.use( express.static(__dirname + '/html' ));
+
+server.get('/hello', function(request, response){
+	console.log('got request on hello');
+	response.send('why hello yourself, sailor');
+})
+
+server.listen(3005, function(){
+	console.log('the server is operational and angry');
+})
